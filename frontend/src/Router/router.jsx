ff1c8601db961f8e8a985c7e702bc05137fffc5b
avronilrajib <avronilrajib@gmail.com>
2024-08-18 12:00:06 +0600

    create home page components about component header component and footer components

--- frontend/src/Router/router.jsx
+++ frontend/src/Router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
+import About from "../pages/About";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
       },
     ],
   },
